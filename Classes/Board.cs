c5d18a8497373b96c04dd925b15666b2d197247c
osamaalkarmi20 <osamaalkarmi20.outlook@outlook.com>
2023-06-21 18:25:27 +0300

    Add project files.

---
+++ Classes/Board.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab04_TicTacToe.Classes
+{
+    class Board
+    {
+        /// <summary>
+        /// Tic Tac Toe Gameboard states
+        /// </summary>
+        public string[,] GameBoard = new string[,]
+        {
+            {"1", "2", "3"},
+            {"4", "5", "6"},
+            {"7", "8", "9"},
+        };
+
+
+        public void DisplayBoard()
+        {
+            
+            for (int row = 0; row < 3; row++)
+            {
+                Console.Write("| ");
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.Write(GameBoard[row, col] + " | ");
+                }
+                Console.WriteLine();
+              
+            }
+        }
+    }
+    }
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using Lab04_TicTacToe.Classes;
+using System;
+
+
+namespace Lab04_TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            StartGame();
+        }
+
+        static void StartGame()
+        {
+
+            Player p1 = new Player();
+            Player p2 = new Player();
+
+            p1.Name = "p1";
+           
+            p1.IsTurn = true;
+            p1.Marker = "X";
+            p2.Name = "p2";
+            p2.Marker = "O";
+          
+            p2.IsTurn = false;
+            Game game1 = new Game(p1, p2);
+            Player Winner = game1.Play();
+            Console.WriteLine(Winner.Name);
+  if (Winner != null)
+            {
+                Console.WriteLine($"{Winner} is winner");
+
+            }
+            else
+            {
+                Console.WriteLine($"there is no winner ");
+
+            }
+
+        }
+       
+
+        // TODO: Setup your game. Create a new method that creates your players and instantiates the game class. Call that method in your Main method.
+        // You are requesting a Winner to be returned, Determine who the winner is output the celebratory message to the correct player. If it's a draw, tell them that there is no winner. 
+
+    }
+
+
+}
